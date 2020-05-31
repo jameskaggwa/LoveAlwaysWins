6ee37bf6bf9476bdaf35cbcb41130af5b50065bd
James Kaggwa <jameskaggwa@gmail.com>
2020-05-31 11:19:55 +0200

    chat menu and file added

--- chat.php
+++ chat.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Causes - LAW</title>
+    <title>Chat - LAW</title>
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic">
@@ -12,23 +12,7 @@
 </head>
 
 <body>
-  <!--
-    <nav class="navbar navbar-light navbar-expand-lg fixed-top" id="mainNav">
-        <div class="container"><a class="navbar-brand" href="index.html"><img src="assets/img/LAW.png"></a><button class="navbar-toggler" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-bars"></i></button>
-            <div
-                class="collapse navbar-collapse" id="navbarResponsive">
-                <ul class="nav navbar-nav ml-auto">
-                  <li class="nav-item" role="presentation"><a class="nav-link" href="index.html">Home</a></li>
-                  <li class="nav-item" role="presentation"><a class="nav-link" href="about.html">About</a></li>
-                  <li class="nav-item" role="presentation"><a class="nav-link" href="causes.html">Causes</a></li>
-                  <li class="nav-item" role="presentation"><a class="nav-link" href="events.html">Events</a></li>
-                  <li class="nav-item" role="presentation"><a class="nav-link" href="gallery.html">Gallery</a></li>
-                  <li class="nav-item" role="presentation"><a class="nav-link" href="blog.html">Blog</a></li>
-                </ul>
-        </div>
-        </div>
-    </nav>
-  -->
+  
     <?php include("menu.html") ?>
 
     <header class="masthead" style="background-image:url('assets/img/school-bg.jpg');">
@@ -37,8 +21,8 @@
             <div class="row">
                 <div class="col-md-10 col-lg-8 mx-auto">
                   <div class="post-heading">
-                    <h1>Our Causes</h1>
-                    <h2 class="subheading">Education in Uganda</h2>
+                    <h1>Let us chat</h1>
+                    <h2 class="subheading">Chat with us</h2>
                   </div>
                 </div>
           </div>
