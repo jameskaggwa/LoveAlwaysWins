1dcf303a0395b11dc30ff8882acfc8af1575406c
James Kaggwa <jameskaggwa@gmail.com>
2020-06-02 08:03:38 +0200

    added impressum-php file

---
+++ impressum.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Chat - LAW</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+</head>
+
+<body>
+
+    <?php include("menu.html") ?>
+
+<!-- header -->
+    <header class="masthead" style="background-image:url('assets/img/school-bg.jpg');">
+        <div class="overlay"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10 col-lg-8 mx-auto">
+                  <div class="post-heading">
+                    <h1>Impressum</h1>
+                    <h2 class="subheading">Impressum</h2>
+                  </div>
+                </div>
+          </div>
+    </header>
+    <!-- header -->
+
+    <!-- content section -->
+    <?php include("impressum.html") ?>
+    <!-- content section -->
+
+
+     <!-- Php include tag -->
+     <?php include("footer.html") ?>
+
+    <!-- js links -->
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/script.min.js"></script>
+</body>
+
+</html>
